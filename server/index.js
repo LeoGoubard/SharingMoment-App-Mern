bab93e672c6971359c05b8416cba8b8d583c1122
LeoG <goubardleo@gmail.com>
2020-12-02 20:41:27 +0100

    the application is now connected with some routes for the API

--- server/index.js
+++ server/index.js
@@ -4,11 +4,23 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+
+app.use('/posts', postRoutes);
+
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, })
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`)))
+    .catch((error) => console.log((error.message)));
+
+mongoose.set('useFindAndModify', false);
