89f7d9490dfd6b84882260803a56b5c014c32c3a
LeoG <goubardleo@gmail.com>
2020-12-04 18:50:41 +0100

    yesy

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
